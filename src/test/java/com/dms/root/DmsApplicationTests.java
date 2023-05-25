326479ca5015247bc2f1401cd8da79758da9cf1b
dhanarajpeddireddy <dhanarajpeddireddy@gmail.com>
2023-05-25 17:45:25 +0530

    first commit

---
+++ src/test/java/com/dms/root/DmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dms.root;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
